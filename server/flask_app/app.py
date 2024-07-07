51b24461924fdffe4739f4efce7ca8a147943e3b
ema-r <ema@localhost.localdomain>
2024-07-07 20:41:29 +0200

    Committer: ema-r <ema@localhost.localdomain>
    Every part of MQTT communication actually connects. Needs fixed callback
    serverside to do something actually useful with the data
    
     On branch dev_e
     Changes to be committed:
    	modified:   server/compose.yaml
    	modified:   server/flask_app/Dockerfile
    	modified:   server/flask_app/app.py
    	modified:   server/flask_app/requirements.txt

--- server/flask_app/app.py
+++ server/flask_app/app.py
@@ -2,50 +2,60 @@ import time
 
 import redis
 from flask import Flask
-from flask_mqtt import Mqtt
+import paho.mqtt.client as mqtt
+
+#from paho.mqtt.enums import CallbackAPIVersion
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+        print("aaa")
+        client.subscribe("pspot/#")
+
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-# Configure mqtt
-app.config['MQTT_BROKER_URL']  = 'localhost' # Change to hostname of mosquitto, after changing mosquitto hostname
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = ''
-app.config['MQTT_PASSWD'] = ''
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False   # Same machine as the mqtt broker
+mqtt_broker_uri = "192.168.178.24"
+mqtt_username = "user2"
+mqtt_passwd = "test"
 
-mqtt = Mqtt(app)
-
-@mqtt.on_connect()
-def handle_connect(client, userdata, flags, rc):
-    mqtt.subscribe("pspot/#")
-
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    data = dict(
-            topic = message.topic,
-            payload = message.payload.decode()
-            )
-
-    split_topic = message.topic.split('/')
-    if split_topic[2] is not None:
-        parking_string = "pid"+split_topic[1]+"_pspot"+split_topic[2]
-        park_value = cache.get(parking_string)
-        if park_value is not None:
-            cache.append(parking_string, -1 * int(park_value))
-        else:
-            cache.append(parking_string, 1)
+#@mqtt.on_message()
+#def handle_mqtt_message(client, userdata, message):
+#    data = dict(
+#            topic = message.topic,
+#            payload = message.payload.decode()
+#            )
+#
+#    split_topic = message.topic.split('/')
+#    if split_topic[2] is not None:
+#        parking_string = "pid"+split_topic[1]+"_pspot"+split_topic[2]
+#        park_value = cache.get(parking_string)
+#        if park_value is not None:
+#            cache.append(parking_string, -1 * int(park_value))
+#        else:
+#            cache.append(parking_string, 1)
 
 # We could upgrade to display each parking lot by filtering their id. We only have one so pid == 1 always
 @app.route('/')
 def hello():
     return 'Hello'
 
-@app.route('/subtest')
+@app.route('/first_setup')
 def stest():
-    mqtt.subscribe('test')
-    return 'okay'
+
+    return 'setup done...'
+
+with app.app_context():
+    mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    mqttc.on_connect = on_connect
+
+    mqttc.username_pw_set(mqtt_username, mqtt_passwd)
+    mqttc.user_data_set([])
+    mqttc.connect(mqtt_broker_uri, 1883, 60)
+    mqttc.loop_start()
+
 
 if __name__ == '__main__':
-   app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000)
+
