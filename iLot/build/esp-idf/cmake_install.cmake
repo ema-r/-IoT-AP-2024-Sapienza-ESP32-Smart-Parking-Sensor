91348583e4cf512ee34e793a59415fba9b12a191
fracchiooo <sconocchiapisoni.1889241@studenti.uniroma1.it>
2024-07-07 23:14:04 +0200

    deleted useful files, changed the certificate subject name

--- iLot/build/esp-idf/cmake_install.cmake
+++ iLot/build/esp-idf/cmake_install.cmake
@@ -447,8 +447,3 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("/home/fracchio/Desktop/-IoT-AP-2024-Sapienza-ESP32-Smart-Parking-Sensor/iLot/build/esp-idf/main/cmake_install.cmake")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/home/fracchio/Desktop/-IoT-AP-2024-Sapienza-ESP32-Smart-Parking-Sensor/iLot/build/esp-idf/nvs_backing/cmake_install.cmake")
-endif()
-
