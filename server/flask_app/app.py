4c00ba9160e3cafa3b6f0cc94c6b48bbce95cb5b
ema-r <ema@localhost.localdomain>
2024-07-08 16:20:12 +0200

    Committer: ema-r <ema@localhost.localdomain>
     Data is now actually added to db, added get_data route to visualize
    every parking spot, plus a "flush_test_data" route to remove data
    leftovers from tests
    
     On branch dev_e
     Changes to be committed:
    	modified:   gateway_cubecell/pc_serial_forwarder.py
    	modified:   server/flask_app/app.py
    	new file:   server/flask_app/templates/spots.html

--- server/flask_app/app.py
+++ server/flask_app/app.py
@@ -1,7 +1,7 @@
 import time
 
 import redis
-from flask import Flask
+from flask import Flask, render_template
 import paho.mqtt.client as mqtt
 
 received = False
@@ -13,16 +13,19 @@ def on_message(client, userdata, message):
             payload = message.payload.decode()
             )
     received = True
-    print('aaa', flush = True)
     print(data['payload'], flush=True)  # Print the payload
-#    split_topic = message.topic.split('/')
-#    if split_topic[2] is not None:
-#        parking_string = "pid"+split_topic[1]+"_pspot"+split_topic[2]
-#        park_value = cache.get(parking_string)
-#        if park_value is not None:
-#            cache.append(parking_string, -1 * int(park_value))
-#        else:
-#            cache.append(parking_string, 1)
+    print(data['topic'], flush=True)
+    split_topic = data['topic'].split('/')
+    if split_topic[2] is not None:
+        parking_string = "pid"+split_topic[1]+"_pspot"+split_topic[2]
+        park_value = cache.get(parking_string)
+        if park_value is None:
+            cache.set(parking_string, "occupied")
+        else:
+            if park_value.decode('UTF-8') == "occupied":
+                cache.set(parking_string, "free")
+            else:
+                cache.set(parking_string, "occupied")
 
 #from paho.mqtt.enums import CallbackAPIVersion
 def on_connect(client, userdata, flags, reason_code, properties):
@@ -45,12 +48,20 @@ mqtt_passwd = "test"
 def hello():
     return 'Hello'
 
-@app.route('/first_setup')
-def stest():
-    if received:
-        return 'nice'
-    else:
-        return 'not nice'
+@app.route('/get_data')
+def ptest():
+    key_values = []
+    for key in cache.scan_iter():
+        if cache.get(key) is not None:
+            key_values.append((key.decode('UTF-8'), cache.get(key).decode('UTF-8')))
+    print(key_values, flush = True)
+    return render_template('spots.html', key_values=key_values)
+
+@app.route('/flush_test_data')
+def flush():
+    for key in cache.scan_iter():
+        cache.delete(key)
+    return 'done'
 
 with app.app_context():
     mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
